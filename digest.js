b21c267caf2e4a1f808ba1a4d48de67958c5fbc1
kavyajadav <kjadhav@stevens.edu>
2023-09-01 21:31:36 +0530

    911

---
+++ digest.js
@@ -0,0 +1,29 @@
+var app = angular.module("app",[]);
+app.controller('emp',['$scope', '$rootScope',function($scope, $rootScope){
+    $scope.a = 1;
+    $scope.b = 2;
+    $scope.c = 3;
+    
+
+    $scope.$watch("a", function(newValue, oldValue){
+        if(newValue != oldValue){
+            console.log("a modified to: "+ newValue)
+        }
+    })
+
+    $scope.$watch("b", function(newValue, oldValue){
+        if(newValue != oldValue){
+            console.log("b modified to: "+ newValue)
+        }
+    })
+
+    $scope.$watch("c", function(newValue, oldValue){
+        if(newValue != oldValue){
+            console.log("c modified to: "+ newValue)
+        }
+    })
+
+    $rootScope.$watch(function(){
+        console.log("--digest process starts--")
+    })
+}])
