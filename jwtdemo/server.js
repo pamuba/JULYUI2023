e105e8c6e84dd0fdec2ecab5bc6be4c9478b79d4
kavyajadav <kjadhav@stevens.edu>
2023-09-12 00:22:24 +0530

    12th SEP 1

---
+++ jwtdemo/server.js
@@ -0,0 +1,49 @@
+require("dotenv").config()
+const express = require("express")
+const jwt = require('jsonwebtoken')
+
+var app = express()
+app.use(express.json())
+
+
+const posts = [
+    {
+        username:"John",
+        title:"Post 1"
+    },
+    {
+        username:"Jim",
+        title:"Post 2"
+    }
+]
+
+app.get('/posts', authenticateToken, (req, res)=>{
+    res.json(posts.filter(post=>post.username === req.user.name))
+});
+
+
+
+app.post('/login', (req, res)=>{
+    //Authicate User
+
+    const username = req.body.username
+    const user = {name:username}
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken:accessToken})
+})
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    // authorization: Bearer TOKEN
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user)=>{
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3001)
