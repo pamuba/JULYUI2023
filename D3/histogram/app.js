40b4d0212bf707258459256f02ce5359e93f8f92
kavyajadav <kjadhav@stevens.edu>
2023-09-27 01:13:08 +0530

    26 Sep 1

--- D3/histogram/app.js
+++ D3/histogram/app.js
@@ -2,8 +2,7 @@ async function draw() {
   // Data
   const dataset = await d3.json('data.json')
 
-  const xAccessor = d =>  d.currently.humidity
-  const yAccessor = d => d.length
+ 
   
   // Dimensions
   let dimensions = {
@@ -26,44 +25,102 @@ async function draw() {
       "transform",
       `translate(${dimensions.margins}, ${dimensions.margins})`
     )
-    //Scales
-    const xScale = d3.scaleLinear()
-            .domain(d3.extent(dataset, xAccessor))
-            .range([0, dimensions.ctrWidth])
-            .nice()
+
+  // console.log({original:dataset, new: newDataset})
+  const labelsGroup = ctr.append('g')
+        .classed('bar-labels', true)
+
+  
+  const xAxisGroup = ctr.append('g')
+                        .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+
+  function histogram(metric){
+    const xAccessor = d =>  d.currently[metric]
+    const yAccessor = d => d.length
+
+        //Scales
+        const xScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0, dimensions.ctrWidth])
+        .nice()
 
     const bin = d3.bin()
-                  .domain(xScale.domain())
-                  .value(xAccessor)
-                  .thresholds(10)
-                
+              .domain(xScale.domain())
+              .value(xAccessor)
+              .thresholds(10)
+            
     const newDataset = bin(dataset)
     const padding = 1
 
     console.log(newDataset)
 
     const yScale = d3.scaleLinear()
-                   .domain([0, d3.max(newDataset, yAccessor)])
-                   .range([dimensions.ctrHeight, 0])
-                   .nice()
-    
-    console.log({original:dataset, new: newDataset})
-    
-    ctr.selectAll('rect')
+               .domain([0, d3.max(newDataset, yAccessor)])
+               .range([dimensions.ctrHeight, 0])
+               .nice()
+
+    const exitTransition = d3.transition().duration(1000)
+    const updateTransition = exitTransition.transition().duration(1000)
+
+    //Bars
+    const temp = ctr.selectAll('rect')
         .data(newDataset)
-        .join('rect')
+        .join(
+          (enter) => enter.append('rect')
+                  .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding ]))
+                  .attr('height' , 0)
+                  .attr('x', d => xScale(d.x0))
+                  .attr('y', d => dimensions.ctrHeight)
+                  .attr('fill', '#b8de6f'),
+          (update)=>update,
+          (exit) => exit.attr('fill', '#f39233')
+          .transition(exitTransition)
+          .attr('y', d => dimensions.ctrHeight)
+          .attr('height', 0)
+          .remove()
+
+        )
+        .transition(updateTransition)
         .attr('width', d => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding ]))
         .attr('height' , d => dimensions.ctrHeight - yScale(yAccessor(d)))
         .attr('x', d => xScale(d.x0))
         .attr('y', d => yScale(yAccessor(d)))
         .attr('fill', '#01c5c4')
+        
 
-  //Axis
-  const xAxis = d3.axisBottom(xScale)
-  const xAxisGroup = ctr.append('g')
-                  .style('transform', `translateY(${dimensions.ctrHeight}px)`)
-      
-  xAxisGroup.call(xAxis)
+    labelsGroup.selectAll('text')
+        .data(newDataset)
+        .join(
+          (enter) => enter.append('text')
+            .attr('x', d=> xScale(d.x0) + (xScale(d.x1) - xScale(d.x0))/2)
+            .attr('y', d=> dimensions.ctrHeight)
+            .text(yAccessor),
+          (update)=>update,
+          (exit) => exit.transition(exitTransition)
+              .attr('y', dimensions.ctrHeight)
+              .remove()
+          
+        )
+        .transition(updateTransition)
+        .attr('x', d=> xScale(d.x0) + (xScale(d.x1) - xScale(d.x0))/2)
+        .attr('y', d=> yScale(yAccessor(d))-10)
+        .text(yAccessor)
+
+    //Axis
+    const xAxis = d3.axisBottom(xScale)    
+    xAxisGroup.transition().call(xAxis)
+
+  }
+
+  //Events
+  d3.select("#metric").on("change", function(e){
+    e.preventDefault()
+    console.log(this)
+    histogram(this.value)
+  })
+
+  histogram('humidity')
+
 }
 
 draw()
